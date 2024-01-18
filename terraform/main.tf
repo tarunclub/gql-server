441eb974579e83eecdee359192cca282823d92f0
tarunclub <tarunkm__@outlook.com>
2024-01-18 12:08:44 +0530

    updated terraform file v3

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,8 @@ resource "aws_instance" "development-instance" {
               #!/bin/bash
               sudo apt update
               sudo apt install -y docker.io
-              sudo docker pull tarunclub/gql-server:1.0.4
-              sudo docker run -d -p 80:5000 tarunclub/gql-server:1.0.4
+              sudo docker pull tarunclub/gql-server:1.0.5
+              sudo docker run -d -p 80:5000 tarunclub/gql-server:1.0.5
               EOF
 
   tags = {
